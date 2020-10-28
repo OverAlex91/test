444fdfc20a9bd0dd2748de235e0ec5e271c76afa
OverAlex91 <onix.dn@gmail.com>
2020-10-28 10:34:12 +0300

    Разобрал добавление переменных, методы remove add и toggle, а так же функцию onclick

--- homework_javascript/js/script.js
+++ homework_javascript/js/script.js
@@ -1,2 +1,9 @@
-console.log(document.querySelector('.page'));
-document.querySelector('.page').classList.remove('second-page');
+let header = document.querySelector('.page');
+let button = document.querySelector('.button_click');
+
+console.log(header);
+console.log(button);
+
+button.onclick = function () {
+    header.classList.toggle('second-page');
+};
