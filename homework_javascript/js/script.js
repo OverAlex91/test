3596a9f74cb6dcfafc103ab50367b423d0c01a90
OverAlex91 <onix.dn@gmail.com>
2020-10-31 17:30:29 +0300

    Выполнение домашнего задания по JS
    Работа с обработчиком событий onclick при клике, onsubmit при отправке формы. Метод textContent

--- homework_javascript/js/script.js
+++ homework_javascript/js/script.js
@@ -4,6 +4,22 @@ let button = document.querySelector('.button_click');
 console.log(header);
 console.log(button);
 
+//обработчик событий onclick
 button.onclick = function () {
+    console.log('Кнопка нажата!');
     header.classList.toggle('second-page');
 };
+
+//блок кода и переменная для работы с методом textContent
+let message = document.querySelector('.text__content');
+console.log(message.textContent);
+
+//блок кода и переменная для работы со свойством onsubmit
+let some_form = document.querySelector('.autorisation_form');
+let onsubmit_message = document.querySelector('.autorisation_form_legend');
+
+some_form.onsubmit = function (event) {
+    event.preventDefault();
+    console.log('Форма отправлена!');
+    onsubmit_message.textContent = 'Форма отправлена!';
+}
