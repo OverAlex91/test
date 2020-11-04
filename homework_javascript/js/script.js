c4baea8b7e6b348912dd8733cb18984a05dbaa76
OverAlex91 <onix.dn@gmail.com>
2020-11-04 12:35:16 +0300

    Работа со скриптами для выполнения домашнего задания Task - 3
    Cтроки 31-33 Вывод данных из полей ввода в консоль Строка 36 - начало скрипта с получением данных из поля ввода и вывод на экран. Строка 49 - начало задачки по приоритетам Cтрока 55 - Cкрипт счетчика

--- homework_javascript/js/script.js
+++ homework_javascript/js/script.js
@@ -17,9 +17,47 @@ console.log(message.textContent);
 //блок кода и переменная для работы со свойством onsubmit
 let some_form = document.querySelector('.autorisation_form');
 let onsubmit_message = document.querySelector('.autorisation_form_legend');
+let input_login = document.getElementById('login');
+let input_password = document.getElementById('password');
 
 some_form.onsubmit = function (event) {
     event.preventDefault();
+
     console.log('Форма отправлена!');
+
+    //замена текста на 'Форма отправлена!'
     onsubmit_message.textContent = 'Форма отправлена!';
+
+    //вывод в консоль значений из input
+    console.log(input_login.value);
+    console.log(input_password.value);
+}
+
+//Получение данных из поля Task 3
+
+let paragraph = document.querySelector('.input_text__content');
+let input_text = document.getElementById('type_text');
+let text_form = document.querySelector('.text_form');
+
+text_form.onsubmit = function (evt) {
+    evt.preventDefault();
+
+    paragraph.textContent = input_text.value;
+}
+
+
+//Задачка по приоритетам Task 3
+
+let x = 55 * (7 + 2) / (4 + 2);
+alert(`Решение задачки по приоритетам - ${x}`);
+
+
+//Счетчик
+let heart = document.querySelector('.heart');
+let likesNumber = document.querySelector('.likes-number');
+let counter = 0;
+heart.onclick = function () {
+    likesNumber.textContent = counter;
+    counter++;
+    heart.classList.toggle('added');
 }
