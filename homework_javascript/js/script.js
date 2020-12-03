a60470aae3c59b462289d041a5c3fa2d1c513455
OverAlex91 <onix.dn@gmail.com>
2020-12-03 18:08:10 +0300

    Выполнение домашнего задание Task - 8

--- homework_javascript/js/script.js
+++ homework_javascript/js/script.js
@@ -1,9 +1,29 @@
-let header = document.querySelector('.page');
-let button = document.querySelector('.button_click');
+'use strict';
+//Реализуйте функцию reverse(), которая переворачивает строку c помощью цикла while
 
-console.log(header);
-console.log(button);
+const reverse = (word) => {
+    let newArr = [];
+    let i = 0;
+    while (i < word.length) {
+        i++;
+        newArr.unshift(word[i - 1]);
+    };
+    let result = newArr.join('');
+    return result;
+}
 
-button.onclick = function () {
-    header.classList.toggle('second-page');
-};
+console.log(reverse('hello, world!'));
+console.log(reverse('I use Brackets'))
+
+//Реализуйте функцию reverse(), которая переворачивает строку c помощью цикла for
+
+const reverseFor = (word) => {
+    let newArr = [];
+    for (let i = 0; i < word.length; i++) {
+        newArr.unshift(word[i]);
+    }
+    return newArr.join('');
+}
+
+console.log(reverseFor('hello, world!'));
+console.log(reverseFor('Learn JavaScript'));
